1335b6d8d26b92ce4a5d1fd441d1d87e5eac0624
Yury Kastsen <yurykstn@gmail.com>
2024-05-01 06:27:10 +0200

    LIGHT

--- src/index.ts
+++ src/index.ts
@@ -2,44 +2,47 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
-import steamCar from '../assets/glb/sedanSports.glb';
-import bg from '../assets/bg.png';
+import sceneModel from '../assets/glb/carJamScene.glb';
+import bg from '../assets/grass.jpg';
 
 
 const scene = new THREE.Scene();
 
-const lightL = new THREE.DirectionalLight( 0xffffff, 1 );
-lightL.position.set( 100, 15, 0 ); //default; light shining from top
+const lightL = new THREE.DirectionalLight( 0xffffff, 0.8 );
+lightL.position.set( 100, 45, 0 );
 lightL.rotateX(100);
 lightL.castShadow = true; // default false
 lightL.shadow.mapSize.width = 512; // default
 lightL.shadow.mapSize.height = 512; // default
 lightL.shadow.camera.near = 0.5; // default
 lightL.shadow.camera.far = 500; // default
+lightL.shadow.radius=0.2;
+lightL.shadow.blurSamples=2;
 scene.add( lightL );
+const lightLHelper = new THREE.DirectionalLightHelper(lightL, 5);
+scene.add(lightLHelper);
 
-const lightR = new THREE.DirectionalLight( 0xffffff, 1 );
-lightR.position.set( -100, 20, 0 ); //default; light shining from top
+const lightR = new THREE.DirectionalLight( 0xffffff, 0.6 );
+lightR.position.set( -100, 40, 0 );
 lightR.rotateX(-100);
-lightR.castShadow = true; // default false
 scene.add( lightR );
-
+const lightRHelper = new THREE.DirectionalLightHelper(lightR, 5);
+scene.add(lightRHelper);
 //Set up shadow properties for the light
 
 
-const lightHelper = new THREE.DirectionalLightHelper(lightR, 5);
-scene.add(lightHelper);
 
-const spotlight = new THREE.SpotLight(0xffffff, 3);
+
+const spotlight = new THREE.SpotLight(0xffffff, 1);
 spotlight.position.set(-130, 400, 10); // позиция света
 spotlight.rotateX(-100);
 spotlight.castShadow = true; // включаем отбрасывание тени
 scene.add(spotlight);
 const spotLightHelper = new THREE.SpotLightHelper(spotlight, 5);
-// scene.add(spotLightHelper);
+scene.add(spotLightHelper);
 
 // Создание AmbientLight
-const ambientLight = new THREE.AmbientLight(0xffffff, 2); // Цвет, Интенсивность
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3); // Цвет, Интенсивность
 scene.add(ambientLight);
 
 var width = window.innerWidth; // Ширина окна браузера
@@ -47,13 +50,14 @@ var height = window.innerHeight; // Высота окна браузера
 var aspectRatio = width / height;
 
 const camera = new THREE.PerspectiveCamera(
-    60,
+    30,
     aspectRatio,
     1,
-    2000);
-camera.position.z = 60;
-camera.position.y = 0;
-camera.lookAt(new THREE.Vector3(0,0,20));
+    1000);
+camera.position.set(33,51,24)
+// camera.position.y = 0;
+// camera.lookAt(new THREE.Vector3(33,41,24));
+camera.rotateX(-Math.PI / 2);
 
 const cameraHelper = new THREE.CameraHelper(camera);
 scene.add(cameraHelper);
@@ -63,7 +67,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
-new OrbitControls(camera, renderer.domElement);
+const orbitControls = new OrbitControls(camera, renderer.domElement);
 
 //creating the plane
 const planeGeometry = new THREE.PlaneGeometry(200, 200);
@@ -82,24 +86,40 @@ scene.add(plane);
 window.onload = function () {
     const gltfLoader = new GLTFLoader();
     
-    gltfLoader.load(steamCar, (gltf) => {
+    gltfLoader.load(sceneModel, (gltf) => {
         const steamCar = gltf.scene;
-        steamCar.scale.set(5, 5, 5);
-        steamCar.position.y = 8.2;
+        steamCar.scale.set(0.5, 0.5, 0.5);
+        steamCar.position.y = -4.7;
         scene.add(steamCar);
         steamCar.add(ambientLight);
-        // steamCar.add(lightL);
+        steamCar.add(lightL);
         steamCar.add(lightR);
         steamCar.add(spotlight);
+
+        steamCar.traverse((child) => {
+            
+            if (child.name) {
+                if (child.name.match('car') || child.name.match('tree')) {
+                    child.castShadow = true;
+                    child.receiveShadow = true;
+                    child.add(lightL);
+                    child.add(lightR);
+                    child.add(ambientLight);
+                    child.add(spotlight);
+                    
+                    child.receiveShadow = false; //default
+                }
+            }
+            
+            
+        })
     });
-    
-    
 };
 
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
-    // camera.aspect = window.innerWidth / window.innerHeight;
+    camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
     render();
